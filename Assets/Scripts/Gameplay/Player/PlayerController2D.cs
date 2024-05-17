af0075a5bd40a3ee432001c12d1cf822db4f3003
Etheko <i.ruizba.2021@alumnos.urjc.es>
2024-05-17 21:16:21 +0200

    Collisions system added for player

--- Assets/Scripts/Gameplay/Player/PlayerController2D.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController2D.cs
@@ -19,11 +19,19 @@ public class PlayerController : MonoBehaviour
 
     private Animator animator;
 
+    private Vector2 startPos;
+
+    private bool hasCollided = false;
+
     public Rigidbody2D rb;
+
+    private int[] collisions = new int[4]; // 0 = up, 1 = down, 2 = left, 3 = right
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        runningSpeed = speed * 2;
+        originalSpeed = speed;
     }
 
 
@@ -49,8 +57,37 @@ public class PlayerController : MonoBehaviour
 
             if (input != Vector2.zero)
             {
+                if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+                {
+                    input.y = 0;
+                }
+                else
+                {
+                    input.x = 0;
+                }
+
+                startPos = transform.position;
                 animator.SetFloat("moveX", input.x);
                 animator.SetFloat("moveY", input.y);
+        
+                // if any of the adjacent cells have a collisionbox, stop the player from moving to that direction
+                if (input.x == 1 && collisions[3] == 1)
+                {
+                    input.x = 0;
+                }
+                else if (input.x == -1 && collisions[2] == 1)
+                {
+                    input.x = 0;
+                }
+                else if (input.y == 1 && collisions[0] == 1)
+                {
+                    input.y = 0;
+                }
+                else if (input.y == -1 && collisions[1] == 1)
+                {
+                    input.y = 0;
+                }
+
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
@@ -62,10 +99,32 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("isWalking", isMoving);
     }
 
-    private void Start()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        runningSpeed = speed * 2;
-        originalSpeed = speed;
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            hasCollided = true;
+            //stop the player from moving
+            StopAllCoroutines();
+            speed = originalSpeed;
+            hasCollided = false;
+            StartCoroutine(Move(startPos));
+
+
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            hasCollided = true;
+            //stop the player from moving
+            StopAllCoroutines();
+            speed = originalSpeed;
+            hasCollided = false;
+            StartCoroutine(Move(startPos));
+
+        }
     }
 
     private void Update()
@@ -76,21 +135,30 @@ public class PlayerController : MonoBehaviour
     IEnumerator Move(Vector3 targetPos)
     {
         isMoving = true;
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon && !hasCollided)
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
             yield return null;
         }
+        var xDiff = startPos.x - targetPos.x;
+        var yDiff = startPos.y - targetPos.y;
         transform.position = targetPos;
         isMoving = false;
+        CheckCollisions();
+        RoundUpAllCoordinates(); // remove coordinates with decimals and round them up
     }
 
-    private bool IsWalkable(Vector3 targetPos)
+    private void RoundUpAllCoordinates()
     {
-        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null)
-        {
-            return false;
-        }
-        return true;
+        transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), 0);
+    }
+
+    private void CheckCollisions() // Check if any of the adjacent cells have a collisionbox (the offset is due to the pivot point of the player)
+    {
+        collisions[0] = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y - 0.5f), new Vector2(0, 0), 0) ? 1 : 0;
+        collisions[1] = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y - 2), new Vector2(0, 0), 0) ? 1 : 0;
+        collisions[2] = Physics2D.OverlapBox(new Vector2(transform.position.x - 1, transform.position.y - 1.5f), new Vector2(0, 0), 0) ? 1 : 0;
+        collisions[3] = Physics2D.OverlapBox(new Vector2(transform.position.x + 1, transform.position.y - 1.5f), new Vector2(0, 0), 0) ? 1 : 0;
+
     }
 }
